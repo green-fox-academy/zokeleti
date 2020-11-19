f9f4529d22d6aedcb25f7c4c1321efb9a3518e4a
zokeleti <keletizoltan0229@gmail.com>
2020-11-19 15:07:55 +0100

    FavoriteNumber and Swap classes added

--- week-01/day-3/printing-to-terminal/src/FavoriteNumber.java
+++ week-01/day-3/printing-to-terminal/src/FavoriteNumber.java
@@ -3,6 +3,6 @@ public class FavoriteNumber {
         int favoriteNumber = 21;
         // Store your favorite number in a variable (as a number)
         // And print it like this: "My favorite number is: 8"
-        System.out.println("My favorite number is " + favoriteNumber );
+        System.out.println("My favorite number is " + favoriteNumber);
     }
 }
