e956ef1224cc2edf59be1d0b33eda0ba3773ab4a
zokeleti <keletizoltan0229@gmail.com>
2020-11-19 16:49:38 +0100

    SecondsInADay class added to w-01 d-3

---
+++ week-01/day-3/printing-to-terminal/src/SecondsInADay.java
@@ -0,0 +1,13 @@
+public class SecondsInADay {
+    public static void main(String[] args) {
+        int currentHours = 14;
+        int currentMinutes = 34;
+        int currentSeconds = 42;
+
+        // Write a program that prints the remaining seconds (as an integer) from a
+        // day if the current time is represented by the variables
+        int fullDay = 24 * 60 * 60;
+        int elapsed = 14 * 60 * 60 + 34 * 60 + 42;
+        System.out.println(fullDay - elapsed);
+    }
+}
