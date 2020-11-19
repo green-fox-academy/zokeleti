90125c1fd729185ddbe70bb27af857dee34c00e4
zokeleti <keletizoltan0229@gmail.com>
2020-11-19 15:24:53 +0100

    DefineBasicInfo class added at w-1 d-3

---
+++ week-01/day-3/printing-to-terminal/src/DefineBasicInfo.java
@@ -0,0 +1,17 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        String name = "Zoltan Keleti";
+        System.out.println(name);
+        // Your age as an integer
+        int age = 32;
+        System.out.println(age);
+        // Your height in meters as a double
+        double height = 1.75;
+        System.out.println(height);
+        // Whether you are married or not as a boolean
+        Boolean maritalStatus = false;
+        System.out.println(maritalStatus);
+    }
+}
