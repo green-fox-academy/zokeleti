4cfcb01f2cfaab052f1cb2e065d1ed7de323e47a
zokeleti <keletizoltan0229@gmail.com>
2020-11-20 11:51:09 +0100

    IWontCheatOnTheExams added

---
+++ week-01/day-3/printing-to-terminal/src/IWontCheatOnTheExams.java
@@ -0,0 +1,9 @@
+public class IWontCheatOnTheExams {
+    // Create a program that writes this line 100 times:
+    // "I won't cheat on the exam!"
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++){
+            System.out.println("I won't cheat on the exam!");
+        }
+    }
+}
