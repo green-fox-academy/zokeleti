f1ebe2e1265ccbde35fd4bde677ec81502b62507
zokeleti <keletizoltan0229@gmail.com>
2020-11-23 14:38:16 +0100

    Doubling added

---
+++ week-02/day-1/src/Doubling.java
@@ -0,0 +1,12 @@
+public class Doubling {
+    public static int Double(int i){
+        return i * 2;
+    }
+    public static void main(String[] args) {
+// - Create an integer variable named `baseNum` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+// - Print the result of `doubling(baseNum)`
+        int baseNum = 123;
+        System.out.println(Double(baseNum));
+    }
+}
