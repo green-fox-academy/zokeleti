701aeb1f72303dac2a5e635694a7b637352627b9
zokeleti <keletizoltan0229@gmail.com>
2020-11-23 13:58:44 +0100

    SumAll added

---
+++ week-02/day-1/src/SumAll.java
@@ -0,0 +1,14 @@
+// - Create an array variable named `numbers`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Print the sum of the elements in `numbers`
+
+public class SumAll {
+    public static void main(String[] args) {
+        int[] numbers = {3, 4, 5, 6, 7};
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++){
+            sum += numbers[i];
+        }
+        System.out.println("The sum of the numbers: " + sum);
+    }
+}
