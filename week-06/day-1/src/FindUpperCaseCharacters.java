1aff9137796e729a8310662f014bd12d53a2a807
zokeleti <keletizoltan0229@gmail.com>
2021-01-04 14:08:28 +0100

    FindUpperCharacters added

--- week-06/day-1/src/FindUpperCaseCharacters.java
+++ week-06/day-1/src/FindUpperCaseCharacters.java
@@ -1,6 +1,27 @@
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 //Write a Stream Expression to find the uppercase characters in a string!
 public class FindUpperCaseCharacters {
     public static void main(String[] args) {
         String string = "SoMeThIng HAppeNeD";
+
+        List<Character> upperCharacters = string
+                .chars()
+                .mapToObj(item -> (char) item)
+                .filter(c -> c.isUpperCase(c))
+                .collect(Collectors.toList());
+
+        for (Character character : upperCharacters){
+            System.out.println(upperCharacters);
+        }
     }
 }
+
+
+/*
+        str.chars()
+        .mapToObj(item -> (char) item)
+        .collect(Collectors.toList())*/
